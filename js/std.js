edb462465b24f981a859e8ed2c230d6c682ca588
Ethan Swan <eswan18@rocketmail.com>
2018-07-29 15:10:11 -0400

    lots of small clenaup

--- js/std.js
+++ js/std.js
@@ -1,5 +1,5 @@
 $(function() {
 	//Set paragraph and header links to open in a new window
 	//(unless they are a 'read more' link)
-	$('.blanktarget').not('a.read_more').attr('target','_blank')
+	$('div.coffee_shops a, div.rec-content a').attr('target','_blank')
 })
