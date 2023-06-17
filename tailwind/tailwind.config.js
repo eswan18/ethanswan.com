36a2ba3b21ddb3c57eb8495c9c55536b02710b60
Ethan Swan <ethanpswan@gmail.com>
2023-06-17 08:52:07 -0500

    Finally fix up nav bar to my liking.

--- tailwind/tailwind.config.js
+++ tailwind/tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: ['../content/**/*.md', '../layouts/**/*.html'],
-  theme: {
-    extend: {},
-  },
+  theme: {},
   plugins: [],
 }
