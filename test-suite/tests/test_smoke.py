9881abbb2a6c9a13b4f1318e64fb220ba1a9c517
Ethan Swan <ethanpswan@gmail.com>
2024-02-26 21:11:50 -0600

    Track dead external links by the original link url.

--- test-suite/tests/test_smoke.py
+++ test-suite/tests/test_smoke.py
@@ -51,17 +51,22 @@ async def test_all_internal_links_work(all_internal_links, client):
 @pytest.mark.asyncio(scope="module")
 async def test_all_external_links_work(all_external_links, client):
     with open(this_dir / 'data' / 'known_dead_links.txt', 'r') as f:
-        lines = [line.split(' ') for line in f.readlines()]
+        lines = [line.strip().split(' ') for line in f.readlines()]
         known_dead_links_and_codes = {line[1]: int(line[0]) for line in lines}
-    tasks = [client.a_get(link, headers=EXTERNAL_HEADERS, follow_redirects=True) for link in all_external_links]
-    dead_links = {}
+    
+    async def get_link(link: str):
+        response = await client.a_get(link, headers=EXTERNAL_HEADERS, follow_redirects=True)
+        return link, response
 
+    tasks = [get_link(link) for link in all_external_links]
+
+    dead_links = {}
     for future in asyncio.as_completed(tasks):
-        response = await future
+        link, response = await future
         # If the response isn't a success, add the link to our dead links set
         if response.status_code != 200:
-            if response.url in known_dead_links_and_codes:
-                if response.status_code == known_dead_links_and_codes[response.url]:
+            if link in known_dead_links_and_codes:
+                if response.status_code == known_dead_links_and_codes[link]:
                     continue
-            dead_links[response.url] = (response.status_code, response.reason_phrase)
+            dead_links[link] = (response.status_code, response.reason_phrase)
     assert not dead_links, f"Dead links found: {dead_links}"
